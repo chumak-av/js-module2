8c47bd5db25c20b7766ab43a827f5fe7f0d44c1e
chumak-av <57865550+chumak-av@users.noreply.github.com>
2020-04-18 16:36:55 +0300

    2

--- module-2/megatask/index.js
+++ module-2/megatask/index.js
@@ -2,7 +2,18 @@ let a = prompt('vvedite koeficient a');
 let b = prompt('vvedite koeficient b');
 let c = prompt('vvedite koeficient c');
 let ac = 4 * a * c;
-const D = Math.round(Math.sqrt(Math.pow(b, 2) - ac));
-let firstX = (-b + D) / 2 * a;
-let secondX = (-b - D) / 2 * a;
-alert(`x1=${ Math.round(firstX)} ,  x2=${ Math.round(secondX)} `);
+let D = Math.pow(b, 2) - ac;
+if (D > 0) {
+    D = Math.sqrt(D);
+    let firstX = (-b + D) / 2 * a;
+    let secondX = (-b - D) / 2 * a;
+    alert(`x1=${ Math.round(firstX)} ,  x2=${ Math.round(secondX)} `);
+} else if (D === 0) {
+    D = Math.sqrt(D);
+    let X = -b / (2 * a);
+    alert(`x=${ Math.round(X)}`);
+} else if (D < 0) {
+    alert("Корней нет");
+} else {
+    alert('Что-то не так');
+}
